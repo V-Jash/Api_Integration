ebf4b063669fee34af9ffb533380e08f9b418348
V-Jash <jashwanthvallabhaneni@gmail.com>
2025-10-14 22:55:45 +0530

    Just changed a big problem in the WeatherResponse

--- WeatherApp/src/model/WeatherResponse.java
+++ WeatherApp/src/model/WeatherResponse.java
@@ -1,15 +1,14 @@
 package model;
 
 public class WeatherResponse {
-    public String latitude;
-    public String longitude;
-    WeatherReport current_weather;
+    public double latitude;
+    public double longitude;
+    public WeatherReport current_weather;
 
-}
-
-class WeatherReport {
-    public String time;
-    public double temperature;
-    public double windspeed;
-    public double winddirection;
+    public static class WeatherReport {
+        public String time;
+        public double temperature;
+        public double windspeed;
+        public double winddirection;
+    }
 }
