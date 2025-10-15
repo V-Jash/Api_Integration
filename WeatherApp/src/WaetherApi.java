2cfcbb75b19a9fa19fcac3755128cc156476bba6
V-Jash <jashwanthvallabhaneni@gmail.com>
2025-10-15 11:42:39 +0530

    The WeatherApp is running successful

--- WeatherApp/src/WaetherApi.java
+++ WeatherApp/src/WaetherApi.java
@@ -2,7 +2,7 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-
+import model.*;
 import com.google.gson.Gson;
 
 public class WaetherApi {
@@ -20,6 +20,15 @@ public class WaetherApi {
 
             // Creating a http response to gather information the external platform
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            Gson gson = new Gson();
+            WeatherResponse weather = gson.fromJson(response.body(), WeatherResponse.class);
+            System.out.println("### WEATHER REPORT ###");
+            System.out.println("Latitude : " + weather.latitude);
+            System.out.println("Longitude : " + weather.longitude);
+            System.out.println("Time : " + weather.current_weather.time);
+            System.out.println("Wind Speed : " + weather.current_weather.windspeed);
+            System.out.println("Wind Direction : " + weather.current_weather.winddirection);
+            System.out.println("Temperature : " + weather.current_weather.temperature);
 
         } catch (Exception e) {
             e.printStackTrace();
