1433e7e0a73d6bf9a8ff14c0fcd8158820cbdd9a
V-Jash <jashwanthvallabhaneni@gmail.com>
2025-10-16 15:57:18 +0530

    Project is working . With minimal errors

---
+++ NewsHeadlineFetcher/src/main/NewsFetcherApp.java
@@ -0,0 +1,17 @@
+package main;
+
+import java.util.*;
+import models.*;
+import models.NewsResponse;
+import service.*;
+import java.io.IOException;
+
+public class NewsFetcherApp {
+    public static void main(String args[]) throws IOException, InterruptedException {
+
+        NewsResponse response = new NewsApiService().fetchTopHeadlines();
+        System.out.println("### Fetching the HeadLines ###");
+        System.out.println("Status code :" + response.status);
+
+    }
+}
